ff54001b2a59290a438c1b3baac723017e0c9365
jlooper <jen.looper@gmail.com>
2017-06-09 21:17:51 -0400

    code cleanup

--- src/app/services/beacon.service.ts
+++ src/app/services/beacon.service.ts
@@ -1,22 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { BluetoothCore } from '@manekinekko/angular-web-bluetooth';
-
+import { Injectable } from "@angular/core";
+import { BluetoothCore } from "@manekinekko/angular-web-bluetooth";
 
 @Injectable()
 export class BeaconService {
-
-  constructor(
-    public ble: BluetoothCore
-  ) {}
+  constructor(public ble: BluetoothCore) {}
 
   getDevice() {
-
     // call this method to get the connected device
     return this.ble.getDevice$();
   }
-
-  
-
 }
